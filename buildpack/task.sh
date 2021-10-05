e65c81e2231b7615f8c018df63a42f46fe1a982e
Shaozhen Ding <shaozhen.ding@smarsh.com>
2021-10-05 06:03:25 -0700

    use native layer

--- buildpack/task.sh
+++ buildpack/task.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for path in "$PWD/cache" "$PWD/layers" "$PWD/source"; do
+mkdir -p /layers 
+mkdir -p /platform
+
+for path in "$PWD/cache" "/layers" "/platform" "$PWD/source"; do
     echo "> Setting permissions on '$path'..."
     chown -R "1000:1000" "$path"
 done
+
 
 CACHE_DIR=$PWD/cache
 CACHE_IMAGE=${APP_IMAGE}-cache
@@ -14,9 +18,9 @@ export CNB_REGISTRY_AUTH="{\"index.docker.io\": \"Basic $(echo -n "${DOCKER_USER
     -cache-dir=${CACHE_DIR} \
     -uid=1000 \
     -gid=1000 \
-    -layers=layers \
-    -platform=platform \
-    -report=layers/report.toml \
+    -layers=/layers \
+    -platform=/platform \
+    -report=/layers/report.toml \
     -process-type=web \
     -skip-restore=${SKIP_RESTORE} \
     -run-image=${RUN_IMAGE} \
